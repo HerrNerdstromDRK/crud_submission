704de43c2a626e28ea59672a8a0592eb182715d0
HerrNerdstromDRK <coding@karrels.com>
2023-01-20 20:28:35 -0600

    Still tweaking hostnames

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 //require("dotenv").config();
 
-const BACKEND_BASE_URL = "http://localhost";
+const BACKEND_BASE_URL = "https://karrels-crud-submission-api.onrender.com";
 const BACKEND_BASE_PORT = 8888;
 
 export default axios.create({
-  baseURL: BACKEND_BASE_URL + ":" + BACKEND_BASE_PORT,
+  baseURL: BACKEND_BASE_URL, // + ":" + BACKEND_BASE_PORT,
 });
